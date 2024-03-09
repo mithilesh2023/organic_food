9da09feeb2e45cb853d0f7b278929a4e1745c1cb
Mithilesh Thakur <119725853+mithilesh2023@users.noreply.github.com>
2024-03-09 12:34:39 +0530

    header complete

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// header scroll
+
+let nav = document.querySelector(".nvabar");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let nvaCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (e) {
+    e.addEventListener("click", function () {
+        nvaCollapse.classList.remove("show")
+    })
+})
